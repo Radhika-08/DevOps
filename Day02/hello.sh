ef3818b11a0bcb24490afddd9084a3d8fe7414f6
Radhika-08 <mansiagrawal7078@gmail.com>
2026-06-15 18:03:34 +0000

    Added Day02 folder

---
+++ Day02/hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "====================================="
+echo "      Welcome to DevOps Day 02 🚀"
+echo "====================================="
+echo "Current User : $(whoami)"
+echo "Date         : $(date)"
+echo "Working Dir  : $(pwd)"
+echo "====================================="
+echo "Let's Learn and Build Something Awesome!"
+echo "====================================="
---
+++ Day02/system_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "===== System Information ====="
+echo "Hostname : $(hostname)"
+echo "Username : $(whoami)"
+echo "Date     : $(date)"
+echo "Uptime   : $(uptime -p)"
